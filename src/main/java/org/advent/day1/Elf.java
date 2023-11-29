f8551553e63b27bcb2ea2a761022972acb2ff89b
Kevin Awator <kekeawa@gmail.com>
2023-11-29 12:34:21 -0500

    exo 1

--- src/main/java/org/advent/day1/Elf.java
+++ src/main/java/org/advent/day1/Elf.java
@@ -15,10 +15,15 @@ public class Elf implements Comparable<Elf> {
         return totalCalories;
     }
 
+    public void addCalories(int calories){
+        totalCalories += calories;
+    }
+    
     @Override
     public int compareTo(Elf arg0) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+        if(this.totalCalories > arg0.totalCalories) return 1;
+        else if(this.totalCalories == arg0.totalCalories) return 0;
+        else return -1;
     }
 
 }
--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -16,21 +16,30 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+       Elf elf = new Elf();
+       elf.addCalories(1000);
+       assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        Elf elf2 = new Elf();
+        elf.addCalories(100);
+        assertThat(elf.compareTo(elf2), equalTo(1));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf elf = new Elf();
+        Elf elf2 = new Elf();
+        assertThat(elf.compareTo(elf2), equalTo(0));    
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
-    }
+        Elf elf = new Elf();
+        Elf elf2 = new Elf();
+        elf2.addCalories(100);
+        assertThat(elf.compareTo(elf2), equalTo(-1));        }
 }
